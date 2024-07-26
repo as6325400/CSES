65fd6c29115b2eb0c991dedda13cd25772fe7c0f
as6325400 <as6325400@gmail.com>
2024-07-27 06:18:57 +0800

    WA school_Dance

---
+++ Graph_Algorithms/School_Dance.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+#define int long long
+
+using namespace std;
+
+class edge{
+  public:
+    int next;
+    int capicity;
+    int rev;
+    edge(int _n, int _c, int _r) : next(_n), capicity(_c), rev(_r){};
+};
+
+vector<vector<edge>> graph;
+
+void add_edge(int a, int b, int capacity){
+  graph[a].push_back(edge(b, capacity, graph[b].size()));
+  graph[b].push_back(edge(a, 0, graph[a].size() - 1));
+}
+
+int dfs(int now, int end, int flow, vector<pair<int, int>> &path, int idx){
+  if(now == end) return flow;
+  auto &e = graph[now][path[idx + 1].second];
+  if(e.capicity > 0){
+    auto ret = dfs(e.next, end, min(flow, e.capicity), path, idx + 1);
+    if(ret > 0){
+      e.capicity -= ret;
+      graph[e.next][e.rev].capicity += ret;
+      return ret;
+    }
+  }
+  return 0;
+}
+
+vector<pair<int, int>> search_path(int start, int end){
+  vector<pair<int, int>> ans;
+  queue<int> q;
+  vector<pair<int, int>> parent(graph.size(), {-1, -1});
+  q.push(start);
+  while(!q.empty()){
+    int now = q.front();
+    q.pop();
+    for(int i = 0; i < (int)graph[now].size(); i++){
+      auto &e = graph[now][i];
+      if(e.capicity > 0 and parent[e.next].first == -1){
+        parent[e.next] = {now, i};
+        if(e.next == end) break;
+        q.push(e.next);
+      }
+    }
+  }
+  if(parent[end].first == -1) return ans;
+  int now = end;
+  while(now != start){
+    auto [node, idx] = parent[now];
+    ans.emplace_back(node, idx);
+    now = node;
+  }
+  ans.emplace_back(start, -1);
+  reverse(ans.begin(), ans.end());
+  return ans;
+}
+
+int maxflow(int start, int end, int node_num){
+  int ans = 0;
+  while(1){
+    vector<bool> visited(node_num + 1, false);
+    auto tmp = search_path(start, end);
+    if(tmp.size() == 0) break;
+    auto flow = dfs(start, end, 1e9, tmp, 0);
+    ans += flow;
+  }
+  return ans;
+}
+
+signed main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  int n, m, k;
+  cin >> n >> m >> k;
+  graph.resize(n + m + 5);
+  for(int i = 0; i < k; i++){
+    int a, b;
+    cin >> a >> b;
+    add_edge(a, n + b, 1);
+  }
+  for(int i = 1; i <= n; i++){
+    add_edge(0, i, 1);
+  }
+  for(int i = 1; i <= m; i++){
+    add_edge(n + i, n + m + 1, 1);
+  }
+  cout << maxflow(0, n + m + 1, 1e9) << '\n';
+  for(int i = 1; i <= n; i++){
+    for(auto &e : graph[i]){
+      if(e.capicity != 0 && e.next != 0){
+        cout << i << ' ' << e.next - n << '\n';
+        break;
+      }
+    }
+  }
+}
