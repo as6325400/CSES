07d7017cb5d1ff9f60f711232f4a4097e4c8a46a
as6325400 <as6325400@gmail.com>
2024-07-27 06:10:00 +0800

    Download_Speed

--- Graph_Algorithms/Download_Speed.cpp
+++ Graph_Algorithms/Download_Speed.cpp
@@ -18,30 +18,57 @@ void add_edge(int a, int b, int capacity){
   graph[b].push_back(edge(a, 0, graph[a].size() - 1));
 }
 
-int dfs(int now, int end, int flow, vector<bool> &visited){
+int dfs(int now, int end, int flow, vector<pair<int, int>> &path, int idx){
   if(now == end) return flow;
-  visited[now] = true;
-  for(int i = 0; i < graph[now].size(); i++){
-    auto &e = graph[now][i];
-    if(e.capicity > 0 and !visited[e.next]){
-      auto ret = dfs(e.next, end, min(flow, e.capicity), visited);
-      if(ret > 0 ){
-        e.capicity -= ret;
-        graph[e.next][e.rev].capicity += ret;
-        return ret;
-      }
+  auto &e = graph[now][path[idx + 1].second];
+  if(e.capicity > 0){
+    auto ret = dfs(e.next, end, min(flow, e.capicity), path, idx + 1);
+    if(ret > 0){
+      e.capicity -= ret;
+      graph[e.next][e.rev].capicity += ret;
+      return ret;
     }
   }
   return 0;
+}
+
+vector<pair<int, int>> search_path(int start, int end){
+  vector<pair<int, int>> ans;
+  queue<int> q;
+  vector<pair<int, int>> parent(graph.size(), {-1, -1});
+  q.push(start);
+  while(!q.empty()){
+    int now = q.front();
+    q.pop();
+    for(int i = 0; i < (int)graph[now].size(); i++){
+      auto &e = graph[now][i];
+      if(e.capicity > 0 and parent[e.next].first == -1){
+        parent[e.next] = {now, i};
+        if(e.next == end) break;
+        q.push(e.next);
+      }
+    }
+  }
+  if(parent[end].first == -1) return ans;
+  int now = end;
+  while(now != start){
+    auto [node, idx] = parent[now];
+    ans.emplace_back(node, idx);
+    now = node;
+  }
+  ans.emplace_back(start, -1);
+  reverse(ans.begin(), ans.end());
+  return ans;
 }
 
 int maxflow(int start, int end, int node_num){
   int ans = 0;
   while(1){
     vector<bool> visited(node_num + 1, false);
-    auto tmp = dfs(start, end, 1e9, visited);
-    if(tmp <= 0) break;
-    ans += tmp;
+    auto tmp = search_path(start, end);
+    if(tmp.size() == 0) break;
+    auto flow = dfs(start, end, 1e9, tmp, 0);
+    ans += flow;
   }
   return ans;
 }
