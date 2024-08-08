05c609371ff810ce79dc7ca8b47bb5c23f2cd861
as6325400 <as6325400@gmail.com>
2024-08-08 16:35:19 +0800

    Round_Trip_ll

--- Graph_Algorithms/Round_Trip_II.cpp
+++ Graph_Algorithms/Round_Trip_II.cpp
@@ -7,72 +7,56 @@ signed main()
 {
   ios_base::sync_with_stdio(false);
   cin.tie(nullptr);
-  int n, m, count = 0;
+  int n, m;
   cin >> n >> m;
-  vector<int> indegree(n + 1, 0);
   vector<vector<int>> graph(n + 1);
   for(int i = 0; i < m; i++){
     int a, b;
     cin >> a >> b;
     graph[a].push_back(b);
-    indegree[b]++;
   }
-  queue<int> q;
-  for(int i = 1; i <= n; i++){
-    if(indegree[i] == 0) q.push(i);
-  }
-  while(!q.empty()){
-    int now = q.front();
-    q.pop();
-    for(int i : graph[now]){
-      indegree[i]--;
-      if(indegree[i] == 0) q.push(i);
-    }
-    count++;
-  }
-  if(count == n) cout << "IMPOSSIBLE\n";
-  else{
-    for(int i = 1; i <= n; i++){
-      if(indegree[i] != 0){
-        q.push(i);
-        break;
-      }
-    }
-    vector<int> visited(n + 1, 0);
-    set<int> s;
-    while(!q.empty()){
-      int now = q.front();
-      q.pop();
-      s.insert(now);
-      for(int i : graph[now]){
-        if(indegree[i] == 0) continue;
-        visited[i] = now;
-        if(s.find(i) == s.end()) q.push(i);
-        else{
-          for(int j = 0; j < n; j++) i = visited[i];
-          stack<int> st;
-          int index = i;
-          st.push(i);
-          do
-          {
-            st.push(visited[index]);
-            index = visited[index];
-          } while (st.top() != i);
-          
-          cout << st.size() << '\n';
-          while(!st.empty()){
-            cout << st.top() << ' ';
-            st.pop();
-          }
-          cout << '\n';
-          for(int i = 1; i <= n; i++){
-            cout << visited[i] << ' ';
-          }
-          return 0;
-        }
-      }
-    }
+  
+  int flag = 1;
 
-    
+  vector<int> color(n + 1, 0);
+  vector<int> pre(n + 1, 0);
+  function<void(int)> dfs = [&](int node){
+    color[node] = 1;
+    for(auto i:graph[node]){
+      if(color[i] == 0){
+        pre[i] = node;
+        dfs(i);
+      }
+      if(color[i] == 1){
+        pre[i] = node;
+        int start = node;
+        for(int j = 0; j < n; j++){
+          start = pre[start];
+        }
+        flag = 0;
+        vector<int> ans;
+        auto temp = start;
+        do{
+          ans.push_back(temp);
+          temp = pre[temp];
+        }while(temp != start);
+        ans.push_back(start);
+        cout << ans.size() << '\n';
+        for(int j = ans.size() - 1; j >= 0; j--){
+          cout << ans[j] << ' ';
+        }
+        exit(0);
+      }
+    }
+    color[node] = 2;
+  };
+
+  for(int i = 1; i <= n; i++){
+    if(color[i] == 0) dfs(i);
   }
+
+  if(flag){
+    cout << "IMPOSSIBLE\n";
+  }
+
 }
