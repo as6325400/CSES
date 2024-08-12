c62ea8faff8aa26138aba2ce62b38e9f565d56d6
as6325400 <as6325400@gmail.com>
2024-08-12 21:10:35 +0800

    Finding Patterns

---
+++ String_Algorithms/Finding_Patterns.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#define int long long
+
+using namespace std;
+
+vector<int> SuffixArray(const string& s) {
+  int n = s.size();
+  vector<int> suffixArray(n), rank(n), tempRank(n);
+  for (int i = 0; i < n; ++i) {
+    suffixArray[i] = i;
+    rank[i] = s[i];
+  }
+  for (int k = 1; k < n; k <<= 1) {
+    auto compare = [&](int i, int j) {
+      if (rank[i] != rank[j])
+        return rank[i] < rank[j];
+      int ri = (i + k < n) ? rank[i + k] : -1;
+      int rj = (j + k < n) ? rank[j + k] : -1;
+      return ri < rj;
+    };
+    sort(suffixArray.begin(), suffixArray.end(), compare);
+    tempRank[suffixArray[0]] = 0;
+    for (int i = 1; i < n; ++i) {
+      tempRank[suffixArray[i]] = tempRank[suffixArray[i - 1]] + compare(suffixArray[i - 1], suffixArray[i]);
+    }
+    rank = tempRank;
+  }
+  return suffixArray;
+}
+
+signed main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  string s1;
+  cin >> s1;
+  auto sa = SuffixArray(s1);
+  int q;
+  cin >> q;
+
+  function<int(string &, int)> cmp = [&](string &str, int idx){
+    int len = s1.size() - idx;
+    for(int i = 0; i < min((int)str.size(), len); i++){
+      if(str[i] != s1[idx + i]){
+        if(str[i] > s1[idx + i]) return 1; // str 大
+        return -1; // SA 大
+      }
+    }
+    if(len < str.size()) return 1;
+    return 0;
+  };
+
+  for(int i = 0; i < q; i++){
+    string str;
+    cin >> str;
+    int l = 0, r = sa.size() - 1, flag = 0;
+    while(l <= r){
+      int mid = (l + r) / 2;
+      if(cmp(str, sa[mid]) == 1) l = mid + 1;
+      else if(cmp(str, sa[mid]) == -1) r = mid - 1;
+      else{
+        cout << "YES\n";
+        flag = 1;
+        break;
+      }
+    }
+    if(!flag) cout << "NO\n";
+  }
+}
